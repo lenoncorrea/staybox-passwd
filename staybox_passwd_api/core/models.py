763a98ac57cd70e470a14b7037c3acbc4660f539
Lenon Corrêa <lenonac_@hotmail.com>
2020-07-22 11:26:05 -0300

    Projeto iniciado e app criada

---
+++ staybox_passwd_api/core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Passwd(models.Model):
+  ip = models.CharField(max_length=255)
+  passwd = models.CharField(max_length=255)
+  datetime = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+      return self.ip + self.passwd
+  
